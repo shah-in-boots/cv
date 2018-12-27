393bea11b21191c10e2d246c5d57680e5d19ddb1
asshah4 <mrshahman@gmail.com>
2018-12-26 22:35:58 -0600

    changed colors around

--- publish.sh
+++ publish.sh
@@ -3,9 +3,3 @@
 #build site from markdown
 jekyll build
 
-# remove old files
-rm -R ../other/blmoore.github.io/cv/*
-
-# re-add new
-cp _site/index.html ../other/blmoore.github.io/cv/.
-cp -R _site/media ../other/blmoore.github.io/cv/.
