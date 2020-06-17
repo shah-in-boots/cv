9020d35db6042048f28edf443606b1155efa0408
Anish Shah <mrshahman@gmail.com>
2020-06-17 14:13:41 -0400

    starting datadrivenCV approach

---
+++ cv.R
@@ -0,0 +1,12 @@
+# Datadriven CV approach
+library(datadrivencv)
+
+# Create CSV files (just run this once)
+#use_csv_data_storage(folder_name = "data", create_output_dir = TRUE)
+
+use_datadriven_cv(
+	full_name = "Anish Sanjay Shah",
+
+	source_location = "https://github.com/asshah4/cv",
+	use_network_logo = FALSE
+)
